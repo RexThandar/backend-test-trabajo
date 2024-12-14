0d0cac2e6f2d315df21a60582c1cdc536cb28a83
Rex Thandar <rex.thandar@gmail.com>
2024-12-13 23:23:20 -0300

    Final de envio, este si final final nueva repo realease 15

--- __tests__/app/cadenas.spec.ts
+++ __tests__/app/cadenas.spec.ts
@@ -1,31 +1,40 @@
-import { contarCoincidenciasEnCadena } from '../../src/app/cadenas';
+import { describe, test } from "@jest/globals";
+import { validarRUT } from '../../src/app/rut';
 
-describe('contarCoincidenciasEnCadena', () => {
-    it('debería contar las coincidencias exactas de una subcadena en una cadena', () => {
-        expect(contarCoincidenciasEnCadena('hola mundo, hola', 'hola')).toBe(1);
+describe('validarRUT', () => {
+    test('debería invalidar un RUT con puntos y guion y un dígito verificador incorrecto', () => {
+        expect(validarRUT('12.345.678-9')).toBe(false);
     });
 
-    it('debería devolver 0 si la subcadena no existe en la cadena', () => {
-        expect(contarCoincidenciasEnCadena('hola mundo', 'adios')).toBe(2);
+    test('debería invalidar un RUT sin puntos ni guion y un dígito verificador incorrecto', () => {
+        expect(validarRUT('123456789')).toBe(false);
     });
 
-    it('debería devolver 0 si la subcadena es más larga que la cadena', () => {
-        expect(contarCoincidenciasEnCadena('hola', 'mundo')).toBe(0);
+    test('debería invalidar un RUT con el dígito verificador "K" incorrecto', () => {
+        expect(validarRUT('12.345.678-K')).toBe(false);
     });
 
-    it('debería contar 1 cuando la subcadena aparece una vez', () => {
-        expect(contarCoincidenciasEnCadena('abcdef', 'cd')).toBe(1);
+    test('debería invalidar un RUT con un dígito verificador incorrecto', () => {
+        expect(validarRUT('12.345.678-1')).toBe(false);
     });
 
-    it('debería devolver 0 si la subcadena es una cadena vacía', () => {
-        expect(contarCoincidenciasEnCadena('hola mundo', '')).toBe(0);
+    test('debería invalidar un RUT con menos de 2 caracteres', () => {
+        expect(validarRUT('1')).toBe(false);
     });
 
-    it('debería devolver 0 si la cadena es vacía', () => {
-        expect(contarCoincidenciasEnCadena('', 'hola')).toBe(0);
+    test('debería invalidar un RUT que contenga caracteres no numéricos', () => {
+        expect(validarRUT('12.34A.678-9')).toBe(false);
     });
 
-    it('debería contar las coincidencias de la subcadena en una cadena con mayúsculas y minúsculas diferentes', () => {
-        expect(contarCoincidenciasEnCadena('Hola Mundo, hola', 'hola')).toBe(1);
+    test('debería validar un RUT sin puntos ni guion con un dígito verificador correcto', () => {
+        expect(validarRUT('12345678-5')).toBe(true);
+    });
+
+    test('debería invalidar un RUT vacío', () => {
+        expect(validarRUT('')).toBe(false);
+    });
+
+    test('debería invalidar un RUT con puntos y guion y el dígito verificador "0" incorrecto', () => {
+        expect(validarRUT('1.234.567-0')).toBe(false);
     });
 });
