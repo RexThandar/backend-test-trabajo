55203a0090a575c1a39e19e12068463576db9088
Rex Thandar <rex.thandar@gmail.com>
2024-12-13 23:19:20 -0300

    Final de envio, este si final final nueva repo realease 13

--- __tests__/app/rut.spec.ts
+++ __tests__/app/rut.spec.ts
@@ -9,7 +9,7 @@ describe('validarRUT', () => {
         expect(validarRUT('12.345.678-1')).toBe(false);
         expect(validarRUT('1')).toBe(false);
         expect(validarRUT('12.34A.678-9')).toBe(false);
-        expect(validarRUT('12345678-5')).toBe(false);
+        expect(validarRUT('12345678-5')).toBe(true);
         // expect(validarRUT('')).toBe(false);
         // expect(validarRUT('1.234.567-0')).toBe(false);
     });
